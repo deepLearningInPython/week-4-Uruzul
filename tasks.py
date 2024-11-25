4b9648db173c8f1077459e2e2a403ab7b1b84487
Adriaan <102516695+Uruzul@users.noreply.github.com>
2024-11-25 16:38:13 +0100

    tasks.py

--- tasks.py
+++ tasks.py
@@ -156,10 +156,11 @@ assert all(id_to_token[token_to_id[key]]==key for key in token_to_id) and all(to
 # Your code here:
 # -----------------------------------------------
 def make_vocabulary_map(documents: list) -> tuple:
-    unique_tokens = sorted(set(word for doc in documents for word in tokenize(doc)))\
+    unique_tokens = sorted(set(word for doc in documents for word in tokenize(doc)))
     token_to_id = {token: idx for idx, token in enumerate(unique_tokens)} 
     id_to_token = {idx: token for token, idx in token_to_id.items()}
     return token_to_it, id_to_token
+
 
 # Test
 t2i, i2t = make_vocabulary_map([text])
